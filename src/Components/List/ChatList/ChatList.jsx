f103c3e9eaf754525ebcf1c2732df8c492d129cd
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-15 01:15:28 +0530

    Completed the List section UI

--- src/Components/List/ChatList/ChatList.jsx
+++ src/Components/List/ChatList/ChatList.jsx
@@ -1,9 +1,97 @@
 import './ChatList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import userProfile from '../../../Assets/Images/userProfile2.png';
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
     return (
         <div className='chat-list'>
-            ChatList
+            <div className="search">
+                <div className="searchBar">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className='faMagnifyingGlass'/>
+                    <input type="text" placeholder='Search...'/>
+                </div>
+            <FontAwesomeIcon icon={addMode ? faMinus : faPlus} className='faPlus' onClick={ () => setAddMode(addMode ? false : true) }/>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src={userProfile} alt="" />
+                <div className="texts">
+                    <span>Divyanshu Kulhadiya</span>
+                    <p>Hey, How are you doing?</p>
+                </div>
+            </div>
         </div>
     )
 }
