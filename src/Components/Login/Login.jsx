dd73012ac568e4ba2a7e136736b6dfc576e2468f
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-15 16:13:55 +0530

    UI Redesign Login Page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,22 @@
 import './Login.css';
+import userProfile from '../../Assets/Images/userProfile2.png';
+import { useState } from 'react';
 
 const Login = () => {
+    const [avatar, setAvatar] = useState({
+        file: null,
+        url: ""
+    })
+
+    const handleAvatar = (e) => {
+        if (e.target.files[0]){
+            setAvatar({
+                file: e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0])
+            })
+        }
+
+    }
     return (
         <div className='login-page'>
             <div className="login">
@@ -15,6 +31,11 @@ const Login = () => {
             <div className="register">
                 <h2>New to Netflix? Sign up now.</h2>
                 <form action="" className='register-form'>
+                    <label htmlFor="file" className='upload-profile'>
+                        <img src={avatar.url || userProfile} alt="" />
+                        Upload profile
+                    </label>
+                    <input type="file" id='file' style={{display:"none"}} onChange={handleAvatar}/>
                     <input type="text" placeholder='Enter username' required/>
                     <input type="email" placeholder='Enter email' required/>
                     <input type="password" placeholder='Enter password' required/>
