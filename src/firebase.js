c96573a298d140e4e3cb0ef71643676ce3a874a5
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-16 01:27:52 +0530

    Integrated firebase into react project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJnK9N1aEA7w6M1S5mJ5-EVsBdJKnTWMI",
+  authDomain: "fir-chat-app-9cf85.firebaseapp.com",
+  projectId: "fir-chat-app-9cf85",
+  storageBucket: "fir-chat-app-9cf85.appspot.com",
+  messagingSenderId: "683711623832",
+  appId: "1:683711623832:web:af827edfa273b55b8196ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
